ec1df7637c674d29716a653595c0fe3af88eabb3
Waleed Sannoufeh <waleedsannoufeh@gmail.com>
2020-01-31 18:54:12 +0000

    updated the program to add full weeks to the calendar

--- mainapp.py
+++ mainapp.py
@@ -38,7 +38,9 @@ duty_fri = pickle.load(load_duty)
 load_duty = open('duty_sat.pkl', 'rb')
 duty_sat = pickle.load(load_duty)
 
-print('\n### This program will enter West Ruislip work duties into your Google Calendar. ###')
+rota_list = pickle.load(open('rota_duty_list.pkl', 'rb'))
+
+print('\n### This program will enter West Ruislip Timetable 70 work duties into your Google Calendar. ###')
 
 # Check if user made a work calendar selection in the past.
 # If not, save the current selection for future use.
@@ -280,7 +282,130 @@ def user_event_input(date_event):
                 print('Format incorrect. Please enter again.\n')
 
 
+def rota_event_input(duty_number, date_event):
+    day = adjust_datetime(str(date_event))[1]
+    while True:
+        if day == 'Sunday':
+            if duty_number == 'r':
+                create_restday_event(date_event)
+                break
+            else:
+                create_sunday_event(duty_number, date_event)
+                break
+
+        elif day == 'Monday':
+            if duty_number == 'r':
+                create_restday_event(date_event)
+                break
+            else:
+                create_monday_thursday_event(duty_number, date_event)
+                break
+
+        elif day == 'Tuesday':
+            if duty_number == 'r':
+                create_restday_event(date_event)
+                break
+            else:
+                create_monday_thursday_event(duty_number, date_event)
+                break
+
+        elif day == 'Wednesday':
+            if duty_number == 'r':
+                create_restday_event(date_event)
+                break
+            else:
+                create_monday_thursday_event(duty_number, date_event)
+                break
+
+        elif day == 'Thursday':
+            if duty_number == 'r':
+                create_restday_event(date_event)
+                break
+            else:
+                create_monday_thursday_event(duty_number, date_event)
+                break
+
+        elif day == 'Friday':
+            if duty_number == 'r':
+                create_restday_event(date_event)
+                break
+            else:
+                create_friday_event(duty_number, date_event)
+                break
+
+        else:
+            if duty_number == 'r':
+                create_restday_event(date_event)
+                break
+            else:
+                create_saturday_event(duty_number, date_event)
+                break
+
+
+def rota_input():
+    print('\n*************************'
+          '\n\nRota Input')
+    while True:
+        while True:
+            try:
+                user_date_input = adjust_datetime(input('\nWeek commencing (eg. 21 Jul 19): '))
+            except IndexError:
+                print('Date format is incorrect. Please enter again.')
+            else:
+                break
+
+        user_date = user_date_input[0]
+        if user_date_input[1] == 'Sunday':
+            while True:
+                rota_number = int(input("Enter starting rota number: (1 to 71) "))
+                if rota_number < 1 or rota_number > 71:
+                    print('Try again.')
+                else:
+                    break
+
+            while True:
+                number_of_weeks = int(input("Number of weeks to enter into calendar: "))
+                if number_of_weeks > 5:
+                    if str.upper(input(f'Number of weeks entered is {number_of_weeks}, are you sure? (y/n): ')) == 'Y':
+                        break
+                    else:
+                        print('\nEnter again.\n')
+                        continue
+                else:
+                    break
+
+            for i in range(number_of_weeks):
+                for ii in range(7):
+                    rota_event_input(rota_list[rota_number][ii+1], user_date)
+                    user_date = user_date + timedelta(days=1)
+                    ii += 1
+                rota_number += 1
+                i += 1
+                if rota_number in [11, 25, 35, 45, 60]:
+                    rota_number += 2
+                    i += 2
+                    user_date = user_date + timedelta(days=14)
+                elif rota_number in [12, 26, 36, 46, 61, 65]:
+                    rota_number += 1
+                    i += 1
+                    user_date = user_date + timedelta(days=7)
+                elif rota_number == 72:
+                    rota_number = 1
+
+        else:
+            print(f'The date {user_date.strftime("%d %b %Y")} is not a Sunday. Please enter again.')
+            continue
+
+        user_continue = str.upper(input('Add duties to other weeks? (y/n): '))
+        if user_continue == 'Y':
+            continue
+        else:
+            break
+
+
 def weekly_input():
+    print('\n*************************'
+          '\n\nWeekly Input')
     while True:
         while True:
             try:
@@ -308,11 +433,13 @@ def weekly_input():
 
 
 def daily_input():
+    print('\n*************************'
+          '\n\nDaily Input')
     while True:
         while True:
             try:
                 user_date_input = adjust_datetime(input('\nDate of duty or rest day (eg. 21 Jul 19): '))
-            except:
+            except IndexError:
                 print('Date format is incorrect. Please enter again.')
             else:
                 break
@@ -329,14 +456,24 @@ def daily_input():
 
 # run the app
 while True:
-    weekly_or_daily = str.upper(input('Weekly or Daily entry? (w/d): '))
-    if weekly_or_daily == 'W':
+    start_selection = input('*************************'
+                            '\n\n1 - Weekly input'
+                            '\n2 - Daily input'
+                            '\n3 - Rota input'
+                            '\n\n4 - Exit'
+                            '\n\nSelect from 1 to 4: ')
+    if start_selection == '1':
         weekly_input()
         break
-    elif weekly_or_daily == 'D':
+    elif start_selection == '2':
         daily_input()
         break
+    elif start_selection == '3':
+        rota_input()
+        break
+    elif start_selection == '4':
+        break
     else:
-        print('Enter W for weekly entry or D for daily entry.')
+        print('Please try again.')
 
 print('\nDuty entry done. Program ended.')
